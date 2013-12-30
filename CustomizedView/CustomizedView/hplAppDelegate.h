7fe2a0a705c1eec4572983676c7ef154ee43fb5f
boycehe <hepeilin1987@qq.com>
2013-12-30 10:54:03 +0800

    New view
    New view

---
+++ CustomizedView/CustomizedView/hplAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  hplAppDelegate.h
+//  CustomizedView
+//
+//  Created by hepeilin on 13-12-27.
+//  Copyright (c) 2013年 minfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hplAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomizedView/CustomizedView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomizedView
+//
+//  Created by hepeilin on 13-12-27.
+//  Copyright (c) 2013年 minfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "hplAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([hplAppDelegate class]));
+    }
+}
